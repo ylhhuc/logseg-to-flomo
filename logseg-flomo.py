18ba2cb956bb7238242e3bf88c3d2c6cbb63c0a5
ylhhuc <35928375+ylhhuc@users.noreply.github.com>
2024-06-24 22:05:57 +0800

    Add files via upload
    用python把logseg的文件逐一导入，并且将[[]]替换成#

---
+++ logseg-flomo.py
@@ -0,0 +1,32 @@
+import os
+import re
+import requests
+
+def send_to_flomo(content):
+    url = 'https://flomoapp.com/iwh/NjEzNzc0/c436dc0981952df313cec0742acbb3de/'
+    headers = {'Content-Type': 'application/json'}
+    payload = {'content': content}
+    response = requests.post(url, json=payload)
+    return response.text
+
+def process_markdown_file(file_path):
+    with open(file_path, 'r', encoding='utf-8') as file:
+        content = file.read()
+    
+    # 替换 [[...]] 为 #...
+    modified_content = re.sub(r'\[\[([^\]]+)\]\]', r'#\1', content)
+    return modified_content
+
+def process_directory(directory):
+    # 遍历目录中的所有Markdown文件
+    for filename in os.listdir(directory):
+        if filename.endswith(".md"):
+            file_path = os.path.join(directory, filename)
+            modified_content = process_markdown_file(file_path)
+            result = send_to_flomo(modified_content)
+            print(f"Processed {filename}: {result}")
+
+# 提供你的Markdown文件所在的文件夹路径
+process_directory(r'E:\logseq\journals\journals')
+
+
